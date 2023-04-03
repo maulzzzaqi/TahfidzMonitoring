99095a5b705b2f702af4d894e5fd564b5e663363
MaulzzZaqi <madezaqi2006@gmail.com>
2023-04-03 13:45:16 +0700

    added login

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,12 @@
 @section('main')
 <div class="border rounded mt-5 mx-auto d-flex flex-column align-items-stretch bg-white" style="width: 500px;">
     <div class="d-flex justify-content-between flex-shrink-0 p-3 link-dark border-bottom">
-        <span class="fs-5 fw-semibold">Welcome to Task Management,</span>
+        @if (Auth::user())
+        <span class="fs-5 text-center fw-semibold">Welcome to Task Management, {{ Auth::user()->name }}!</span>
         <span class="btn btn-sm btn-primary">Add</span>
+        @else
+        <span class="fs-5 text-center fw-semibold">Welcome to Task Management!</span>
+        @endif
     </div>
 </div>
 @endsection
